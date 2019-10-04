5000e3b5443c78253be650293d3194f82d2ef257
Kenny Grannas <s526521@nwmissouri.edu>
2019-10-04 14:54:51 -0500

    Added intent for Time Converter

--- app/src/main/java/com/example/basecalc/MainActivity.java
+++ app/src/main/java/com/example/basecalc/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.basecalc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent ini = null;
+        switch (v.getId()){
+            case R.id.btnTimeConverter:
+                ini = new Intent(this, TimeConerter.class);
+                break;
+        }
+        startActivity(ini);
     }
 }
