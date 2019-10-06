1c726390aecb0d042ffe987e8cc0cfb58fc87df6
Samuel Eagan <samueleagan@gmail.com>
2019-10-05 21:46:30 -0500

    Implemented OnItemSelectedListener and created onItemSelected method

--- app/src/main/java/com/example/basecalc/CoversionActivity.java
+++ app/src/main/java/com/example/basecalc/CoversionActivity.java
@@ -3,12 +3,20 @@ package com.example.basecalc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
 
-public class CoversionActivity extends AppCompatActivity {
+public class CoversionActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coversion);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapter, View v, int n, long l) {
+
     }
 }
