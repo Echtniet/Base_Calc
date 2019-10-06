e90b6c985199c4d4f1d79d2028d05474dc30297f
Alex <37240461+ales11112@users.noreply.github.com>
2019-10-06 16:48:15 -0500

    Add spinner functionality to calculator

--- app/src/main/java/com/example/basecalc/Calulator.java
+++ app/src/main/java/com/example/basecalc/Calulator.java
@@ -3,12 +3,32 @@ package com.example.basecalc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 
-public class Calulator extends AppCompatActivity {
+public class Calulator extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calulator);
+        String[] baseArray = {"Base 2", "Base 8", "Base 10", "Base 16"};
+        Spinner mathBase = findViewById(R.id.sprBase);
+        int layout = R.layout.support_simple_spinner_dropdown_item;
+        ArrayAdapter<String> dropDown = new ArrayAdapter<>(this, layout, baseArray);
+        mathBase.setOnItemSelectedListener(this);
+        mathBase.setAdapter(dropDown);
+    }
+    @Override
+    public void onItemSelected(AdapterView<?> adapter, View v, int pos, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapter) {
+
     }
 }
