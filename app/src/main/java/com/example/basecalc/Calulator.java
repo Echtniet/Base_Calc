0112db473f9cdfc735a9daf83a2ffd0ebbe94a1b
Alex <37240461+ales11112@users.noreply.github.com>
2019-10-23 15:52:51 -0500

    add onClick for items in Calculator

--- app/src/main/java/com/example/basecalc/Calulator.java
+++ app/src/main/java/com/example/basecalc/Calulator.java
@@ -1,13 +1,16 @@
 package com.example.basecalc;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.GridView;
 import android.widget.Spinner;
-import android.widget.SpinnerAdapter;
+import android.widget.TextView;
 
 public class Calulator extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     public int base;
@@ -23,6 +26,7 @@ public class Calulator extends AppCompatActivity implements AdapterView.OnItemSe
         mathBase.setOnItemSelectedListener(this);
         mathBase.setAdapter(dropDown);
     }
+
     @Override
     public void onItemSelected(AdapterView<?> adapter, View v, int pos, long id) {
         if(pos == 0){
@@ -39,5 +43,86 @@ public class Calulator extends AppCompatActivity implements AdapterView.OnItemSe
     @Override
     public void onNothingSelected(AdapterView<?> adapter) {
 
+    }
+
+    public void onItemClick(View v) {
+        int viewId = v.getId();
+        switch (viewId) {
+            case (R.id.btn0):
+
+                break;
+            case (R.id.btn1):
+
+                break;
+            case (R.id.btn2):
+
+                break;
+            case (R.id.btn3):
+
+                break;
+            case (R.id.btn4):
+
+                break;
+            case (R.id.btn5):
+
+                break;
+            case (R.id.btn6):
+
+                break;
+            case (R.id.btn7):
+
+                break;
+            case (R.id.btn8):
+
+                break;
+            case (R.id.btn9):
+
+                break;
+            case (R.id.btnA):
+
+                break;
+            case (R.id.btnB):
+
+                break;
+            case (R.id.btnC):
+
+                break;
+            case (R.id.btnD):
+
+                break;
+            case (R.id.btnE):
+
+                break;
+            case (R.id.btnF):
+
+                break;
+            case (R.id.btnClear):
+
+                break;
+            case (R.id.btnPM):
+
+                break;
+            case (R.id.btnPren):
+
+                break;
+            case (R.id.btnDivide):
+
+                break;
+            case (R.id.btnMul):
+
+                break;
+            case (R.id.btnMinus):
+
+                break;
+            case (R.id.btnPlus):
+
+                break;
+            case (R.id.btnEquals):
+
+                break;
+            case (R.id.btnDot):
+
+                break;
+        }
     }
 }
