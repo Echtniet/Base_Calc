eb3b7230c0ca1d56e6c305e234a55cb9ceb1982a
Alex <37240461+ales11112@users.noreply.github.com>
2019-10-23 21:30:19 -0500

    Add onClick case statements and recolor method

--- app/src/main/java/com/example/basecalc/Calulator.java
+++ app/src/main/java/com/example/basecalc/Calulator.java
@@ -12,8 +12,11 @@ import android.widget.GridView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 public class Calulator extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
-    public int base;
+    private int base;
+    private int[] ctrlInfo ={0};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +29,21 @@ public class Calulator extends AppCompatActivity implements AdapterView.OnItemSe
         ArrayAdapter<String> dropDown = new ArrayAdapter<>(this, layout, baseArray);
         mathBase.setOnItemSelectedListener(this);
         mathBase.setAdapter(dropDown);
+        mathBase.setSelection(2);
         Log.d("CALC-DEBUG", "onCreate: End");
     }
 
     @Override
     public void onItemSelected(AdapterView<?> adapter, View v, int pos, long id) {
+        TextView display = findViewById(R.id.txtResult);
+        String displayText = display.getText().toString();
         if(pos == 0){
+            switch (base){
+                case (8):
+
+                    break;
+                case (10):
+            }
             base = 2;
         }else if(pos == 1){
             base = 8;
@@ -43,9 +55,27 @@ public class Calulator extends AppCompatActivity implements AdapterView.OnItemSe
     }
 
     private void recolorItems(){
+        Button[] btnArr = {
+                findViewById(R.id.btn2),
+                findViewById(R.id.btn3),
+                findViewById(R.id.btn4),
+                findViewById(R.id.btn5),
+                findViewById(R.id.btn6),
+                findViewById(R.id.btn7),
+                findViewById(R.id.btn8),
+                findViewById(R.id.btn9),
+                findViewById(R.id.btnA),
+                findViewById(R.id.btnB),
+                findViewById(R.id.btnC),
+                findViewById(R.id.btnD),
+                findViewById(R.id.btnE),
+                findViewById(R.id.btnF)
+        };
         switch (base) {
             case (2):
-
+                for(int i = 0; i < btnArr.length; i++){
+//                    btnArr[i]
+                }
                 break;
             case (8):
 
@@ -67,57 +97,105 @@ public class Calulator extends AppCompatActivity implements AdapterView.OnItemSe
     public void onItemClick(View v) {
         TextView outputText = findViewById(R.id.txtResult);
         int viewId = v.getId();
+        if(ctrlInfo[0] == 0){
+            outputText.setText("");
+        }
         switch (viewId) {
             case (R.id.btn0):
                 outputText.setText(outputText.getText().toString() + "0");
+                ctrlInfo[0]++;
                 break;
             case (R.id.btn1):
                 outputText.setText(outputText.getText().toString() + "1");
+                ctrlInfo[0]++;
                 break;
             case (R.id.btn2):
+                if(base == 2){
+                    break;
+                }
                 outputText.setText(outputText.getText().toString() + "2");
+                ctrlInfo[0]++;
                 break;
             case (R.id.btn3):
+                if(base == 2){
+                    break;
+                }
                 outputText.setText(outputText.getText().toString() + "3");
+                ctrlInfo[0]++;
                 break;
             case (R.id.btn4):
+                if(base == 2){
+                    break;
+                }
                 outputText.setText(outputText.getText().toString() + "4");
+                ctrlInfo[0]++;
                 break;
             case (R.id.btn5):
+                if(base == 2){
+                    break;
+                }
                 outputText.setText(outputText.getText().toString() + "5");
+                ctrlInfo[0]++;
                 break;
             case (R.id.btn6):
+                if(base == 2){
+                    break;
+                }
                 outputText.setText(outputText.getText().toString() + "6");
+                ctrlInfo[0]++;
                 break;
             case (R.id.btn7):
+                if(base == 2){
+                    break;
+                }
                 outputText.setText(outputText.getText().toString() + "7");
+                ctrlInfo[0]++;
                 break;
             case (R.id.btn8):
+                if(base == 2 || base == 8){
+                    break;
+                }
                 outputText.setText(outputText.getText().toString() + "8");
+                ctrlInfo[0]++;
                 break;
             case (R.id.btn9):
+                if(base == 2 || base == 8){ break; }
                 outputText.setText(outputText.getText().toString() + "9");
+                ctrlInfo[0]++;
                 break;
             case (R.id.btnA):
+                if(base != 16){ break; }
                 outputText.setText(outputText.getText().toString() + "a");
+                ctrlInfo[0]++;
                 break;
             case (R.id.btnB):
+                if(base != 16){ break; }
                 outputText.setText(outputText.getText().toString() + "b");
+                ctrlInfo[0]++;
                 break;
             case (R.id.btnC):
+                if(base != 16){ break; }
                 outputText.setText(outputText.getText().toString() + "c");
+                ctrlInfo[0]++;
                 break;
             case (R.id.btnD):
+                if(base != 16){ break; }
                 outputText.setText(outputText.getText().toString() + "d");
+                ctrlInfo[0]++;
                 break;
             case (R.id.btnE):
+                if(base != 16){ break; }
                 outputText.setText(outputText.getText().toString() + "e");
+                ctrlInfo[0]++;
                 break;
             case (R.id.btnF):
+                if(base != 16){ break; }
                 outputText.setText(outputText.getText().toString() + "f");
+                ctrlInfo[0]++;
                 break;
             case (R.id.btnClear):
-
+                outputText.setText("");
+                ctrlInfo[0] = 0;
                 break;
             case (R.id.btnPM):
 
