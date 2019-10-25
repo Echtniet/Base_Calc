f5141d21b1858b6efe1b71e792f5bcbe4a3711eb
Samuel Eagan <samueleagan@gmail.com>
2019-10-25 14:00:20 -0500

    Readded Log statement that Clinton removed and then added button

--- app/src/main/java/com/example/basecalc/CoversionActivity.java
+++ app/src/main/java/com/example/basecalc/CoversionActivity.java
@@ -8,12 +8,13 @@ import android.view.View;
 import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.lang.reflect.Array;
 
-public class CoversionActivity extends AppCompatActivity  {
+public class CoversionActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +25,22 @@ public class CoversionActivity extends AppCompatActivity  {
 
         Spinner convertFrom = findViewById(R.id.sprFrom);
         Spinner convertTo = findViewById(R.id.sprFrom2);
+
+        Log.d("Conversion Activity", "Activity Creation Finished");
+
+        final Button convertButton = findViewById(R.id.btnConvert);
+        convertButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                
+            }
+        });
+
     }
 
+    public void onClick(View v) {
+        if (v.getId() == R.id.btnConvert) {
 
+        }
+    }
 }
